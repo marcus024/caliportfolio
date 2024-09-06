a31945bbf71145ddd39fae53882375ad5e304850
calipayanhub <114867908+calipayanhub@users.noreply.github.com>
2024-09-07 01:15:53 +0800

    Add hover effects

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import About from './about/about.js';
 import Project from './projects/projects.js';
 import Contacts from './contacts/contacts.js';
 import Skills from './skills/skills.js';
-
+import './App.css';
 const App = () => {
   return (
     <div>
-      <nav style={{ position: 'fixed', top: 0, width: '100%', backgroundColor: '#333' }}>
-        <ul style={{ display: 'flex', justifyContent: 'space-around', listStyleType: 'none', padding: '10px' }}>
-          <li><a href="#home" style={{ color: '#fff' }}>Home</a></li>
-          <li><a href="#skills" style={{ color: '#fff' }}>Skills</a></li>
-          <li><a href="#project" style={{ color: '#fff' }}>Project</a></li>
-          <li><a href="#about" style={{ color: '#fff' }}>About</a></li>
-          <li><a href="#contacts" style={{ color: '#fff' }}>Contacts</a></li>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><a href="#home" >Home</a></li>
+          <li><a href="#skills" >Skills</a></li>
+          <li><a href="#project" >Project</a></li>
+          <li><a href="#about" >About</a></li>
+          <li><a href="#contacts">Contacts</a></li>
         </ul>
       </nav>
       <Home />
       <Skills />
-      <About />
       <Project />
+      <About />
       <Contacts />
     </div>
   );
